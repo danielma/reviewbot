858a80df241cd26ab37a7720262f2f334631a00c
Daniel Ma <drailskid@yahoo.com>
2017-05-15 08:49:55 -0700

    Cut 0.5.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v0.5.0
+
+- don't send notifications if noone is working (2017-05-15)
+
 # v0.4.0
 
 - support bamboo HR to see when people are out (2017-05-05)
--- lib/review_bot.rb
+++ lib/review_bot.rb
@@ -16,5 +16,5 @@ require_relative 'review_bot/notification'
 require_relative 'review_bot/bamboo_hr'
 
 module ReviewBot
-  VERSION = '0.4.0'
+  VERSION = '0.5.0'
 end
