ad3d996dd5b7c659999834ce8ca953fb0a78bfa0
Greg Losie <glosie@gmail.com>
2018-08-22 13:55:50 -0700

    use github_api to make reviews request

--- lib/review_bot/pull_request_review.rb
+++ lib/review_bot/pull_request_review.rb
@@ -1,16 +1,11 @@
 module ReviewBot
   class PullRequestReview < Hashie::Mash
     def self.for_pull_request(pull_request)
-      # github_api doesn't support this yet
-      conn = Faraday.new(
-        url: 'https://api.github.com',
-        headers: { Accept: 'application/vnd.github.black-cat-preview+json' }
-      )
-      reviews_json = conn.get(
-        "/repos/#{pull_request.repo_owner}/#{pull_request.repo_name}/pulls/#{pull_request.number}/reviews?access_token=#{ENV['GH_AUTH_TOKEN']}"
-      ).body
+      reviews = GH.pull_requests
+                  .reviews
+                  .list(pull_request.repo_owner, pull_request.repo_name, pull_request.number)
 
-      JSON.parse(reviews_json).map { |r| new r }
+      reviews.map { |r| new r }
     end
 
     def approved?
